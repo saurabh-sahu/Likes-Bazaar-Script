51ac74833ba4f2b4c7b6aa68835929cace7b92f4
saurabh-sahu <saurabhsahu.nitr@gmail.com>
2017-05-10 18:29:00 +0530

    fb page and photo

--- tests/FBLikerTest.php
+++ tests/FBLikerTest.php
@@ -10,14 +10,16 @@ class FBLikerTest extends PHPUnit_Extensions_Selenium2TestCase
         $this->setBrowserUrl('https://www.facebook.com');
     }
 
-    const URLS = [
+    const PHOTO_URLS = [
         'https://www.facebook.com/photo.php?fbid=708423979318875&set=a.103364529824826.7563.100004538376348&type=3&theater',
-        'https://www.facebook.com/photo.php?fbid=755335437913094&set=a.100385076741470.686.100003099331858&type=3&theater',
+    ];
+
+    const PAGE_URLS = [
+        'https://www.facebook.com/Harshi-Srivastava-1857529834534345',
     ];
 
     const ACCOUNTS = [
         'juhi.sharma12@rediffmail.com' => '7809863931',
-        'raju.shahzan@rediffmail.com'  => '7809863931',
     ];
 
     public function testRecursive()
@@ -34,7 +36,16 @@ class FBLikerTest extends PHPUnit_Extensions_Selenium2TestCase
         $this->byName('email')->value($email);
         $this->byName('pass')->value($password);
         $this->clickOnElement('loginbutton');
-        foreach (self::URLS as $url) {
+        $this->likePageList();
+        $this->likePhotoList();
+        $this->clickOnElement("userNavigationLabel");
+        sleep(1);    
+        $this->byLinkText('Log out')->click();
+    }
+
+    public function likePhotoList()
+    {
+        foreach (self::PHOTO_URLS as $url) {
             $this->url($url);
             sleep(1);
             $this->keys('l'.Keys::ENTER);
@@ -46,9 +57,17 @@ class FBLikerTest extends PHPUnit_Extensions_Selenium2TestCase
             $this->keys(Keys::ESCAPE);
             sleep(0.5);
         }
-        $this->clickOnElement("userNavigationLabel");
-        sleep(1);    
-        $this->byLinkText('Log out')->click();
     }
+
+    public function likePageList()
+    {
+        foreach (self::PAGE_URLS as $url) {
+            $this->url($url);
+            sleep(1);
+            $this->keys(Keys::ESCAPE);
+            $this->byClassName('likeButton')->click();
+        }
+    }
+
 }
 ?>
