25479e2d14d3d2d3d6496cbadfb4b703874289bd
saurabh-sahu <saurabhsahu.nitr@gmail.com>
2017-05-10 15:41:22 +0530

    made testas proper

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'cards'),
+            'database' => env('DB_DATABASE', 'likesbazaar'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
--- tests/FBLikerTest.php
+++ tests/FBLikerTest.php
@@ -1,5 +1,6 @@
 <?php
 use PHPUnit_Extensions_Selenium2TestCase_Keys as Keys;
+use Illuminate\Support\Facades\DB;
 
 class FBLikerTest extends PHPUnit_Extensions_Selenium2TestCase
 {
@@ -10,69 +11,28 @@ class FBLikerTest extends PHPUnit_Extensions_Selenium2TestCase
     }
 
     const URLS = [
-    'https://www.facebook.com/photo.php?fbid=755335437913094&set=a.100385076741470.686.100003099331858&type=3&theater',//54
-    //'https://www.facebook.com/photo.php?fbid=585693944877245&set=a.100385076741470.686.100003099331858&type=3&theater',//34
-    //'https://www.facebook.com/photo.php?fbid=291350624311580&set=a.100385076741470.686.100003099331858&type=3&theater',//17
+        'https://www.facebook.com/photo.php?fbid=708423979318875&set=a.103364529824826.7563.100004538376348&type=3&theater',
+        'https://www.facebook.com/photo.php?fbid=755335437913094&set=a.100385076741470.686.100003099331858&type=3&theater',
     ];
 
-    const EMAILS = [
-        'saurabhsahu474@gmail.com', 
-        'saurabhsahu.nitr+ne@gmail.com', 
-        '7809863931', 
-        //'raju.shahzan@rediffmail.com',
-        //'juhi.sharma12@rediffmail.com',
-        // 'Raju.shahzan@outlook.com',//   auth prob
-        // 'rahul.raj.sharma1993@yahoo.com', // auth prob
-        // 'abhishek.anand1989@yahoo.com', // auth prob 
-        // 'ruthfrost474@gmail.com',// 1 auth prob 
-        // 'ruthfrost474@yahoo.com',// 2
-        // 'ruthfrost474@reddif.com',// 3 reddif password: rockingcool
-        // 'ruthfrost474@outlook.com',// 4 outlook pasword: rockingcooldude_1
-        //'ruthfrost@zoho.com',//not happening
-        //'saurabhsahu474@yahoo.com',//not happening
+    const ACCOUNTS = [
+        'juhi.sharma12@rediffmail.com' => '7809863931',
+        'raju.shahzan@rediffmail.com'  => '7809863931',
     ];
 
-    const PASS = [
-        'rockingcooldude', 
-        'rockingcooldude', 
-        '7809863931',
-        // '7809863931',
-        // '7809863931',
-        // '7809863931',
-        // 'rockingcooldude', 
-        // 'rockingcooldude', 
-        // 'rockingcooldude', 
-        // 'rockingcooldude', 
-        // 'rockingcooldude', 
-        // 'rockingcooldude',
-        //'rockingcooldude', 
-        //'rockingcooldude',
-    ];
-
-    public function testFacebookLogin1()
+    public function testRecursive()
     {
-        $this->url('https://www.facebook.com'); 
-        $this->byName('email')->value(self::EMAILS[0]);
-        $this->byName('pass')->value(self::PASS[0]);
-        $this->clickOnElement('loginbutton');
-        echo $this->active();
-        foreach (self::URLS as $url) {
-            $this->url($url);
-            sleep(1);
-            $this->keys('l'.Keys::ENTER);
-            sleep(1);  
-            $this->keys(Keys::ESCAPE);
-            sleep(1);
-            $this->keys('l'.Keys::ENTER);
-            sleep(1);    
+        foreach (self::ACCOUNTS as $email => $password) {
+            $this->FBForRec($email, $password);
         }
     }
 
-    public function testFacebookLogin2()
+    public function FBForRec($email, $password)
     {
         $this->url('https://www.facebook.com'); 
-        $this->byName('email')->value(self::EMAILS[1]);
-        $this->byName('pass')->value(self::PASS[1]);
+        $this->byName('email')->clear();
+        $this->byName('email')->value($email);
+        $this->byName('pass')->value($password);
         $this->clickOnElement('loginbutton');
         foreach (self::URLS as $url) {
             $this->url($url);
@@ -83,185 +43,12 @@ class FBLikerTest extends PHPUnit_Extensions_Selenium2TestCase
             sleep(1);
             $this->keys('l'.Keys::ENTER);
             sleep(1);    
-        }
-    }
-
-    public function testFacebookLogin3()
-    {
-        $this->url('https://www.facebook.com'); 
-        $this->byName('email')->value(self::EMAILS[2]);
-        $this->byName('pass')->value(self::PASS[2]);
-        $this->clickOnElement('loginbutton');
-        foreach (self::URLS as $url) {
-            $this->url($url);
-            sleep(1);
-            $this->keys('l'.Keys::ENTER);
-            sleep(1);  
             $this->keys(Keys::ESCAPE);
-            sleep(1);
-            $this->keys('l'.Keys::ENTER);
-            sleep(1);    
+            sleep(0.5);
         }
+        $this->clickOnElement("userNavigationLabel");
+        sleep(1);    
+        $this->byLinkText('Log out')->click();
     }
-
-    public function testFacebookLogin4()
-    {
-        $this->url('https://www.facebook.com'); 
-        $this->byName('email')->value(self::EMAILS[3]);
-        $this->byName('pass')->value(self::PASS[3]);
-        $this->clickOnElement('loginbutton');
-        foreach (self::URLS as $url) {
-            $this->url($url);
-            sleep(1);
-            $this->keys('l'.Keys::ENTER);
-            sleep(1);  
-            $this->keys(Keys::ESCAPE);
-            sleep(1);
-            $this->keys('l'.Keys::ENTER);
-            sleep(1);    
-        }
-    }
-
-    public function testFacebookLogin5()
-    {
-        $this->url('https://www.facebook.com'); 
-        $this->byName('email')->value(self::EMAILS[4]);
-        $this->byName('pass')->value(self::PASS[4]);
-        $this->clickOnElement('loginbutton');
-        foreach (self::URLS as $url) {
-            $this->url($url);
-            sleep(1);
-            $this->keys('l'.Keys::ENTER);
-            sleep(1);  
-            $this->keys(Keys::ESCAPE);
-            sleep(1);
-            $this->keys('l'.Keys::ENTER);
-            sleep(1);    
-        }
-    }
-
-    // public function testFacebookLogin6()
-    // {
-    //     $this->url('https://www.facebook.com'); 
-    //     $this->byName('email')->value(self::EMAILS[5]);
-    //     $this->byName('pass')->value(self::PASS[5]);
-    //     $this->clickOnElement('loginbutton');
-    //     foreach (self::URLS as $url) {
-    //         $this->url($url);
-    //         sleep(1);
-    //         $this->keys('l'.Keys::ENTER);
-    //         sleep(1);  
-    //         $this->keys(Keys::ESCAPE);
-    //         sleep(1);
-    //         $this->keys('l'.Keys::ENTER);
-    //         sleep(1);    
-    //     }
-    // }
-
-    // public function testFacebookLogin7()
-    // {
-    //     $this->url('https://www.facebook.com'); 
-    //     $this->byName('email')->value(self::EMAILS[6]);
-    //     $this->byName('pass')->value(self::PASS[6]);
-    //     $this->clickOnElement('loginbutton');
-    //     foreach (self::URLS as $url) {
-    //         $this->url($url);
-    //         sleep(1);
-    //         $this->keys('l'.Keys::ENTER);
-    //         sleep(1);  
-    //         $this->keys(Keys::ESCAPE);
-    //         sleep(1);
-    //         $this->keys('l'.Keys::ENTER);
-    //         sleep(3);    
-    //     }
-    // }
-
-    // public function testFacebookLogin8()
-    // {
-    //     $this->url('https://www.facebook.com'); 
-    //     $this->byName('email')->value(self::EMAILS[7]);
-    //     $this->byName('pass')->value(self::PASS[7]);
-    //     $this->clickOnElement('loginbutton');
-    //     foreach (self::URLS as $url) {
-    //         $this->url($url);
-    //         sleep(1);
-    //         $this->keys('l'.Keys::ENTER);
-    //         sleep(1);  
-    //         $this->keys(Keys::ESCAPE);
-    //         sleep(1);
-    //         $this->keys('l'.Keys::ENTER);
-    //         sleep(3);    
-    //     }
-    // }
-    // public function testFacebookLogin9()
-    // {
-    //     $this->url('https://www.facebook.com'); 
-    //     $this->byName('email')->value(self::EMAILS[8]);
-    //     $this->byName('pass')->value(self::PASS[8]);
-    //     $this->clickOnElement('loginbutton');
-    //     foreach (self::URLS as $url) {
-    //         $this->url($url);
-    //         sleep(1);
-    //         $this->keys('l'.Keys::ENTER);
-    //         sleep(1);  
-    //         $this->keys(Keys::ESCAPE);
-    //         sleep(1);
-    //         $this->keys('l'.Keys::ENTER);
-    //         sleep(3);    
-    //     }
-    // }
-    // public function testFacebookLogin10()
-    // {
-    //     $this->url('https://www.facebook.com'); 
-    //     $this->byName('email')->value(self::EMAILS[9]);
-    //     $this->byName('pass')->value(self::PASS[9]);
-    //     $this->clickOnElement('loginbutton');
-    //     foreach (self::URLS as $url) {
-    //         $this->url($url);
-    //         sleep(1);
-    //         $this->keys('l'.Keys::ENTER);
-    //         sleep(1);  
-    //         $this->keys(Keys::ESCAPE);
-    //         sleep(1);
-    //         $this->keys('l'.Keys::ENTER);
-    //         sleep(3);    
-    //     }
-    // }
-    // public function testFacebookLogin11()
-    // {
-    //     $this->url('https://www.facebook.com'); 
-    //     $this->byName('email')->value(self::EMAILS[10]);
-    //     $this->byName('pass')->value(self::PASS[10]);
-    //     $this->clickOnElement('loginbutton');
-    //     foreach (self::URLS as $url) {
-    //         $this->url($url);
-    //         sleep(1);
-    //         $this->keys('l'.Keys::ENTER);
-    //         sleep(1);  
-    //         $this->keys(Keys::ESCAPE);
-    //         sleep(1);
-    //         $this->keys('l'.Keys::ENTER);
-    //         sleep(3);    
-    //     }
-    // }
-
-    // public function testFacebookLogin12()
-    // {
-    //     $this->url('https://www.facebook.com'); 
-    //     $this->byName('email')->value(self::EMAILS[11]);
-    //     $this->byName('pass')->value(self::PASS[11]);
-    //     $this->clickOnElement('loginbutton');
-    //     foreach (self::URLS as $url) {
-    //         $this->url($url);
-    //         sleep(1);
-    //         $this->keys('l'.Keys::ENTER);
-    //         sleep(1);  
-    //         $this->keys(Keys::ESCAPE);
-    //         sleep(1);
-    //         $this->keys('l'.Keys::ENTER);
-    //         sleep(3);    
-    //     }
-    // }
-
 }
 ?>
